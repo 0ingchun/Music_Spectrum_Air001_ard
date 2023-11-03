5e172a73b56738a392385382129d19277c99af43
0ingchun <Oingchun@outlook.com>
2023-11-03 20:33:06 +0800

    增加了按钮开关屏幕功能

---
+++ README.md
@@ -0,0 +1,36 @@
+# Air001_Music_Spectrum
+## 材料 
+1. 麦克风（有模拟信号输出即可）：MAX4466
+2. 亚克力板：67 x 67 x 2 mm 黑茶色 透明非磨砂
+3. Ws2812灯板： 65 x 65 mm  8 x 8 = 64 LEDs
+4. 均光：普通硫酸纸或者用A4纸
+5. MCU：合宙Air001
+6. 按钮(可输出高低电平)：触摸或自复位按钮
+
+## 注意事项
+ 1. ws2812B灯珠的排列方向不是蛇形走位，如果买到蛇形走位的，需要在程序中稍作修改
+ 2. 软件顶部工具栏，clock source and Frequency把Air001主频调到HSI 24 MHz，HCLK 48MHz的组合。
+ 3. 更改代码优化等级，顶部工具栏optimize修改为第二项（smallest（-OS）with LTO），不然会flash over flow
+ 4. ws2812b链接vbus或5v，不要连接3.3v
+
+## 关于亮度的问题
+> 可以在myws2812.cpp文件中等比例修改除数的值，从而改变灯的亮度,例如:
+```cpp
+void myWs2812b::SetLedColor(uint8_t ledid, uint8_t r, uint8_t g, uint8_t b) {
+  // 这里可以更改后面的数值调节亮度，不除的话，亮度比较高，发热比较严重
+  LED_MATRIX[ledid][0] = r/5;
+  LED_MATRIX[ledid][1] = g/5;
+  LED_MATRIX[ledid][2] = b/5;
+}
+```
+
+## 接线
+ws2182灯板：PA_7
+按钮：PB_3
+麦克风：PA_0
+
+## 鸣谢/Thanks
+
+本项目为 【weiruitong123】大佬的 https://github.com/weiruitong123/Air001_Music_Spectrum Air001声音频谱屏 项目的略微修改版本，外壳和arduino库文件全来自【weiruitong123】大佬，目前仅添加利用按钮开关屏幕功能，计划使用按钮调节亮度
+
+![image2](E:\Document\Code\OtherProject\Air001_Music_Spectrum\图片\image2.jpg)
---
+++ 程序/Air001_Music_UV/myws2812b.cpp
@@ -0,0 +1,74 @@
+#include "myws2812b.h"
+#include "SPI.h"
+#define CODE_1 (0xF8)
+#define CODE_0 (0xC0)
+uint8_t brightness = 50;
+static uint8_t LED_MATRIX[64][3] = { 0 };
+void myWs2812b::Writebit(uint8_t bit) {
+  
+  if (bit) {
+    SPI.transfer(CODE_1);
+  } else {
+    SPI.transfer(CODE_0);
+  }
+}
+
+void myWs2812b::WriteColor(uint8_t r, uint8_t g, uint8_t b) {
+  for (uint8_t i = 0; i < 8; i++) {
+    Writebit(g >> (7 - i));
+  }
+  for (uint8_t i = 0; i < 8; i++) {
+    Writebit(r >> (7 - i));
+  }
+  for (uint8_t i = 0; i < 8; i++) {
+    Writebit(b >> (7 - i));
+  }
+}
+void myWs2812b::Show() {
+  for (uint8_t i = 0; i < 64; i++) {
+    WriteColor(LED_MATRIX[i][0], LED_MATRIX[i][1], LED_MATRIX[i][2]);
+  }
+  Reset();
+}
+void myWs2812b::Clear() {
+
+  for (uint8_t i = 0; i < 64; i++) {
+    LED_MATRIX[i][0] = 0;
+    LED_MATRIX[i][1] = 0;
+    LED_MATRIX[i][2] = 0;
+    WriteColor(0, 0, 0);
+  }
+  Reset();
+}
+void myWs2812b::Reset() {
+  for (int i = 0; i < 5; i++) {
+    SPI.transfer(0);
+  }
+}
+void myWs2812b::SetLedColor(uint8_t ledid, uint8_t r, uint8_t g, uint8_t b) {
+  // 这里可以更改后面的数值调节亮度，不除的话，亮度比较高，发热比较严重
+  LED_MATRIX[ledid][0] = r/5;
+  LED_MATRIX[ledid][1] = g/5;
+  LED_MATRIX[ledid][2] = b/5;
+}
+
+
+// void myWs2812b::SetLedColorBrightness(uint8_t ledid, uint8_t r, uint8_t g, uint8_t b) {
+//   // 根据亮度值调整每个颜色分量
+//   // 全局亮度变量，范围从0（完全关闭）到255（最大亮度）
+//   uint8_t brightness = 50; // 例如，设置为50的亮度级别
+//   LED_MATRIX[ledid][0] = (uint16_t)r * brightness / 255;
+//   LED_MATRIX[ledid][1] = (uint16_t)g * brightness / 255;
+//   LED_MATRIX[ledid][2] = (uint16_t)b * brightness / 255;
+// }
+
+
+
+
+
+
+
+
+
+
+
